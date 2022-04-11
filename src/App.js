c3132bd58b25182499e68fcd7886fd58dd34e407
Mahamud Hasan <mahmudhasan960458@gmail.com>
2022-04-12 03:14:48 +0600

    Create Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {CssBaseline, Grid} from '@material-ui/core';
+import Header from '../src/components/Header/Header.jsx';
+import List from '../src/components/List/List.jsx';
+import Map from '../src/components/Map/Map.jsx';
+import PlaceDetails from '../src/components/PlaceDetails/PlaceDetails.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+const App = () =>{
+    return(
+       <>
+           
+       </>
+
+    )
+};
 
 export default App;
